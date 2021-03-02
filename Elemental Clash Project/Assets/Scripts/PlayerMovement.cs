a6993adadef45d523a75276e6be938253fcfeb98
DeanGartshore <0516507@fife.ac.uk>
2021-03-02 19:07:53 +0000

    added graphics and player movement

---
+++ Elemental Clash Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // public varible visible in unity
+    public float forceStrenght;
+
+    // theses functions will be called upon by each button for movement
+   public void MoveRight()
+    {
+        // get rigidbody for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        // move in right dirction
+        ourRigidbody.AddForce(Vector2.right * forceStrenght);
+
+    }
+
+
+
+   public void MoveLeft()
+    {
+        // get rigidbody for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        // move in right dirction
+        ourRigidbody.AddForce(Vector2.left * forceStrenght);
+
+
+    }
+
+
+}
